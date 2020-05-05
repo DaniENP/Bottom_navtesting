9f614588b9af47f88da673037e98b4050a96b698
DaniENP <64818983+DaniENP@users.noreply.github.com>
2020-05-04 21:27:44 -0500

    Cambiopvto

--- app/src/main/java/com/example/bottom_nav/MainActivity.kt
+++ app/src/main/java/com/example/bottom_nav/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     // Sebas es pvto
+    // sebas tambien es pvto
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
